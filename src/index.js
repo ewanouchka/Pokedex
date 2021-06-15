998aff878aad9b8da88cd892b13dba04c77e6f7a
ewanouchka <ewanouchka@gmail.com>
2021-06-15 22:28:12 +0200

    Mise en forme CSS

--- src/index.js
+++ src/index.js
@@ -1,75 +1,87 @@
-/* 
-  You may use any method of set/update your application
-  below is just an example
-*/
-
 let pokemons = [];
 
-let { id, name, type, img } = {
+// un pokemon implémenté pour la base (à revoir pour boucler à travers la liste de pokémons par la suite)
+
+let aPokemon = {
   id: 1,
   name: "bulbizarre",
-  type: "plant",
+  type: "plante",
   img:
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
 };
 
+// transformer la liste en array
+
 const loadPokemon = () => {
-  pokemons.push(id);
-  pokemons.push(name);
-  pokemons.push(type);
-  pokemons.push(img);
+  pokemons.push(aPokemon.id);
+  pokemons.push(aPokemon.name);
+  pokemons.push(aPokemon.type);
+  pokemons.push(aPokemon.img);
 
   return pokemons;
 };
 
 pokemons = loadPokemon();
 
+// bouton clic
+
 const app = document.querySelector("#app");
 
 app.addEventListener("click", () => {
+  let noMatch = document.getElementById("pokeList");
+  noMatch.remove();
+
   let listOfPokemons = document.createElement("div");
-  listOfPokemons.id = "pokemonItem";
-  let listOfPokemons_content = document.createTextNode(pokemons);
-  listOfPokemons.appendChild(listOfPokemons_content);
-
-  let emptyList = document.getElementById("pokeList");
-  let parentDiv = emptyList.parentNode;
-
-  parentDiv.replaceChild(listOfPokemons, emptyList);
+  listOfPokemons.id = "pokemonItem"; 
+  listOfPokemons.className = "pokemonItem"; // on crée une div à l'intérieur de laquelle mettre les infos du pokemon
 
   let pokeId = document.createElement("div");
-  pokeId.id = "pokemonId";
-  let pokeId_content = document.createTextNode(pokemons[0]);
+  pokeId.className = "pokemonId";
+  let pokeId_content = document.createTextNode("#"+pokemons[0]); // on crée une div pour l'ID du pokemon, qui reprend la première valeur de l'array
   pokeId.appendChild(pokeId_content);
-  let pokeName = document.createElement("div");
-  pokeName.id = "pokemonName";
-  let pokeName_content = document.createTextNode(pokemons[1]);
-  pokeName.appendChild(pokeName_content);
-  let pokeType = document.createElement("div");
-  pokeType.id = "pokemonType";
-  let pokeType_content = document.createTextNode(pokemons[2]);
-  pokeType.appendChild(pokeType_content);
-  let pokeImg = document.createElement("div");
-  pokeImg.id = "pokemonImg";
-  let pokeImg_content = new Image(100, 100);
-  pokeImg_content.src = pokemons[3];
 
+  let pokeName = document.createElement("div");
+  pokeName.className = "pokemonName";
+  let pokeName_content = document.createTextNode(pokemons[1]); // on crée une div pour le nom du pokemon, qui reprend la deuxième valeur de l'array
+  pokeName.appendChild(pokeName_content);
+
+  let pokeType = document.createElement("div");
+  pokeType.className = "pokemonType";
+  let pokeType_content = document.createTextNode(pokemons[2]); // on crée une div pour le type du pokemon, qui reprend la troisième valeur de l'array
+  pokeType.appendChild(pokeType_content);
+
+  let pokeImg = document.createElement("div");
+  pokeImg.className = "pokemonImg"; // on crée une div pour l'image du pokemon
+  let pokeImg_content = new Image(100, 100); // on crée un contenu image
+  pokeImg_content.src = pokemons[3]; // on récupère la quatrième valeur de l'array pour le lien URL de l'image
   pokeImg.appendChild(pokeImg_content);
 
-  let oldList = document.getElementById("pokemonItem");
-  let newPokemonItem = oldList.parentNode;
-  newPokemonItem.replaceChild(pokeId, oldList);
-  newPokemonItem.append(pokeName);
-  newPokemonItem.append(pokeType);
-  newPokemonItem.append(pokeImg);
+  let emptyList = document.getElementById("app"); // on appelle la div qui contient "no pokemon found"
+  let parentDiv = emptyList.parentNode;
+
+  parentDiv.replaceChild(listOfPokemons, emptyList); // on la remplace par le bloc extérieur
+
+  let oldList = document.getElementById("pokemonItem"); // on insère les infos à l'intérieur du bloc
+  let newPokemonItem = oldList;
+    newPokemonItem.append(pokeId, pokeName, pokeType, pokeImg);
 });
 
-/*
-const handleLoadPokemons = (listOfPokemon) => {
-  pokemons = listOfPokemon;
-};*/
+
+// import "./styles.css";
+
+/* 
+  You may use any method of set/update your application
+  below is just an example
+
+let pokémons = [];
+*/
 
 // function to call on click when <button>Load Pokémons</button> is press
+
+/* const handleLoadPokémons = (listOfPokémon) => {
+  pokémons = listOfPokémon;
+};
+*/
 
 // 1 Should set pokémons on click using addEventlister and/or onclick property
 
@@ -86,7 +98,10 @@ a pokémon is of type : {
 
 // 2 Should replace <p>no pokémon found<p> with a list of pokémon
 
-/*
+/*app.innerHTML = `<h1>Liste de Pokémons:</h1>
+                <button>Load Pokémons</button>
+                <p>no pokémon found</p>`;
+
 // 3 Should looks great !!! use ur own imagination !
 
 /* 4 how about using a real pokémon api ? 
